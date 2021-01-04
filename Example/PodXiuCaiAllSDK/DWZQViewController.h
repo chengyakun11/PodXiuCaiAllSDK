86d0b504daca6f34890c9dc76d6500e4c495f67c
kent <chengyakun11@163.com>
2021-01-04 17:08:25 +0800

    Initial commit

---
+++ Example/PodXiuCaiAllSDK/DWZQViewController.h
@@ -0,0 +1,13 @@
+//
+//  DWZQViewController.h
+//  PodXiuCaiAllSDK
+//
+//  Created by kent on 01/04/2021.
+//  Copyright (c) 2021 kent. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DWZQViewController : UIViewController
+
+@end
---
+++ Example/PodXiuCaiAllSDK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PodXiuCaiAllSDK
+//
+//  Created by kent on 01/04/2021.
+//  Copyright (c) 2021 kent. All rights reserved.
+//
+
+@import UIKit;
+#import "DWZQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DWZQAppDelegate class]));
+    }
+}
